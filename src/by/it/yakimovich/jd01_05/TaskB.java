efb3e1591e290a9bf6f0b1cb3f1d45d15414aa3b
nikit <nikitayakim@gmail.com>
2019-09-07 07:31:14 +0300

    1-5 Tasks

--- src/by/it/yakimovich/jd01_05/TaskB.java
+++ src/by/it/yakimovich/jd01_05/TaskB.java
@@ -1,5 +1,7 @@
 package by.it.yakimovich.jd01_05;
 
+import static java.lang.Math.*;
+
 public class TaskB {
     public static void main(String[] args) {
         step1();
@@ -7,8 +9,12 @@ public class TaskB {
     }
 
     private static void step1() {
-        for (double a = 0; a <2; a=+0.2) {
-
+        for (double a = 0; a <=2; a+=0.2) {
+            double y=0;
+            for (int x = 1; x <=6; x++) {
+                y+=(pow(7,a)- cos(x));
+                System.out.printf("%s%.2f%s%f\n" , "a =", a, "sum y", y);
+            }
         }
     }
 
