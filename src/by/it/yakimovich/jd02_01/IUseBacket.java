ee1765a8fcd7c9784ab83076605ba8f622327ef5
nikit <nikitayakim@gmail.com>
2019-10-06 21:39:22 +0300

    2с4с5с6с10с

---
+++ src/by/it/yakimovich/jd02_01/IUseBacket.java
@@ -0,0 +1,6 @@
+package by.it.yakimovich.jd02_01;
+
+public interface IUseBacket {
+    void takeBacket();
+    void putGoodsToBacket();
+}
---
+++ src/by/it/yakimovich/jd02_02/iBuyer.java
@@ -0,0 +1,11 @@
+package by.it.yakimovich.jd02_02;
+
+public interface iBuyer {
+    void enterToMarket();
+
+    void chooseGoods();
+
+    void goToQueue();
+
+    void goOut();
+}
