437ab0762b596207f5fa2128db0b655c4628a182
krautsevich <mihail4egevara@gmail.com>
2019-10-08 15:07:32 +0200

    jd02_04 TaskB

--- src/by/it/krautsevich/Test/Message.java
+++
@@ -1,6 +0,0 @@
-package by.it.krautsevich.Test;
-
-class Message {
-    static String WELCOME = "message.welcome";
-    static String QUESTION = "message.question";
-}
--- src/by/it/krautsevich/Test/User.java
+++
@@ -1,6 +0,0 @@
-package by.it.krautsevich.Test;
-
-public class User {
-    static String FIRSTNAME = "user.firstname";
-    static String LASTNAME = "user.lastname";
-}
