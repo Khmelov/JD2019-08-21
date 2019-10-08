c6d40f94088d4816146f647c5d88e27a189c448b
Ekaterina Zlotnikova <e.zlotnikova@gmail.com>
2019-10-08 15:19:49 +0300

    [ezlotnikova] 02-06 A

---
+++ src/by/it/zlotnikova/jd02_06/ClientThread.java
@@ -0,0 +1,14 @@
+package by.it.zlotnikova.jd02_06;
+
+public class ClientThread extends Thread {
+
+    public ClientThread(String name) {
+        this.setName(name);
+    }
+
+    @Override
+    public void run() {
+        Logger logger = Logger.getLogger();
+        logger.log("I am " + this.getName());
+    }
+}
