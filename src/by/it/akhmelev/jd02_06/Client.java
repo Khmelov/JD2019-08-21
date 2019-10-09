7f76a14dc0ef62137ab285add937395ed8b99ee4
loktev <loktev@tut.by>
2019-10-09 07:29:03 +0300

    classwork 02_06

---
+++ src/by/it/akhmelev/jd02_06/Client.java
@@ -0,0 +1,13 @@
+package by.it.akhmelev.jd02_06;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Logger logger=Logger.getLogger();
+        logger.log("I main thread");
+        for (int i = 0; i < 5; i++) {
+            new ClientThread("th" + i).start();;
+        }
+    }
+
+}
---
+++ src/by/it/akhmelev/jd02_06/PathGenerator.java
@@ -0,0 +1,14 @@
+package by.it.akhmelev.jd02_06;
+
+import java.io.File;
+
+class PathGenerator {
+    static String getPath(Class<?> taskAClass) {
+        String rootProject = System.getProperty("user.dir");
+        String subPath = taskAClass.getName()
+                .replace(".", File.separator)
+                .replace(taskAClass.getSimpleName(), "");
+        return rootProject + File.separator + "src" + File.separator + subPath;
+    }
+
+}
