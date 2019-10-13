ccb77c20bb296fd401fb00f33882746903a1bb33
Arseniy <36487649+ArsiKerzh@users.noreply.github.com>
2019-10-14 00:15:06 +0300

    jd02_04(task A-B)jd02_06(taskA)

---
+++ src/by/it/boytsov/jd02_06/Exceptions.java
@@ -0,0 +1,10 @@
+package by.it.boytsov.jd02_06;
+
+public class Exceptions extends Exception{
+
+
+
+    public Exceptions(String message) {
+        super(message);
+    }
+}
---
+++ src/by/it/boytsov/jd02_06/PathGenerator.java
@@ -0,0 +1,12 @@
+package by.it.boytsov.jd02_06;
+
+import  java.io.File;
+
+public class PathGenerator {
+    static String directory(Class<?> cl) {
+        String path = System.getProperty("user.dir") + File.separator + "src" + File.separator;
+        String classDirectory = cl.getName().replace(cl.getSimpleName(), "").replace(".", File.separator);
+        return path + classDirectory;
+    }
+}
+
